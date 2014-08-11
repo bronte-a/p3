734c7df3920525fb876a6ed5143cc71ff2df256b
Bronte Adshead <bronte.adshead@43379.auckland.natcoll.ac.nz>
2014-08-11 16:32:59 +1200

    first add

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+if($(window).width() >= 450){
+  // do your stuff
+
+    $( ".work1" ).click(function() {
+      $("#work1").fadeIn( 700 );
+    });
+
+    $( "#close1" ).click(function() {
+      $("#work1").fadeOut( 700 );
+    });
+
+    $( ".work2" ).click(function() {
+      $("#work2").fadeIn( 700 );
+    });
+
+    $( "#close2" ).click(function() {
+      $("#work2").fadeOut( 700 );
+    });
+
+    $( ".work3" ).click(function() {
+      $("#work3").fadeIn( 700 );
+    });
+
+    $( "#close3" ).click(function() {
+      $("#work3").fadeOut( 700 );
+    });
+
+    $( ".work4" ).click(function() {
+      $("#work4").fadeIn( 700 );
+    });
+
+    $( "#close4" ).click(function() {
+      $("#work4").fadeOut( 700 );
+    });
+
+    $( ".work5" ).click(function() {
+      $("#work5").fadeIn( 700 );
+    });
+
+    $( "#close5" ).click(function() {
+      $("#work5").fadeOut( 700 );
+    });
+
+    $( ".work6" ).click(function() {
+      $("#work6").fadeIn( 700 );
+    });
+
+    $( "#close6" ).click(function() {
+      $("#work6").fadeOut( 700 );
+    });
+
+    $( ".work7" ).click(function() {
+      $("#work7").fadeIn( 700 );
+    });
+
+    $( "#close7" ).click(function() {
+      $("#work7").fadeOut( 700 );
+    });
+
+}
